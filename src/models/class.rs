3793653a14ba058a53899236a91a37d8f4c4145e
Ibrahim Alshekh <ibrahim.shekh.m@gmail.com>
2026-02-18 16:59:01 +0100

    Remove `add_subjects` method from `Class` model and add unit tests for `add_subject` and `remove_subject` methods.

--- src/models/class.rs
+++ src/models/class.rs
@@ -38,15 +38,33 @@ impl Class {
         &self.subjects
     }
 
-    pub fn add_subjects(&mut self, subjects: IndexSet<SubjectId>) {
-        self.subjects.extend(subjects);
-    }
-
     fn add_subject(&mut self, subject_id: SubjectId) {
         self.subjects.insert(subject_id);
     }
 
     fn remove_subject(&mut self, subject_id: SubjectId) {
         self.subjects.retain(|id| id != &subject_id);
+    }
+}
+
+#[cfg(test)]
+mod class_tests {
+    use super::*;
+
+    #[test]
+    fn subject_can_be_added_to_class() {
+        let mut class = Class::new(1, "Test Class".to_string(), "Test Description".to_string());
+        class.add_subject(1);
+        assert_eq!(class.subject_ids().len(), 1);
+        assert!(class.subject_ids().contains(&1));
+    }
+
+    #[test]
+    fn subject_can_be_removed_from_class() {
+        let mut class = Class::new(1, "Test Class".to_string(), "Test Description".to_string());
+        class.add_subject(1);
+        class.remove_subject(1);
+        assert_eq!(class.subject_ids().len(), 0);
+        assert!(!class.subject_ids().contains(&1));
     }
 }
